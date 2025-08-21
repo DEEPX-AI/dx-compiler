5eaeb2b59a1ba6b925ff828cbcef924573c1191a
DX-CI <dci@deepx.ai>
2025-08-21 18:59:20 +0900

    compiler-20250821-rc2+manual.sha.c5d8cb4

--- scripts/common_util.sh
+++ scripts/common_util.sh
@@ -33,6 +33,7 @@ print_colored_v2() {
     print_colored "$2" "$1"
 }
 
+
 check_container_mode() {
     # Check if running in a container
     if grep -qE "/docker|/lxc|/containerd" /proc/1/cgroup || [ -f /.dockerenv ]; then
@@ -52,5 +53,39 @@ check_virtualenv() {
     else
         print_colored_v2 "❌ No virtual environment is currently active."
         return 1
+    fi
+}
+
+
+# Handle command failure with user confirmation and suggested action
+handle_cmd_failure() {
+    local error_message=$1
+    local hint_message=$2
+    local origin_cmd=$3
+    local suggested_action_cmd=$4
+    
+    print_colored_v2 "ERROR" "${error_message}"
+    print_colored_v2 "HINT" "${hint_message}"
+    print_colored_v2 "YELLOW" "Would you like to perform the suggested action now? [Y/n] (Default is 'y' after 10 seconds of no input. This process will be aborted if you enter 'n')"
+    read -t 10 -p ">> " user_input
+    user_input=${user_input:-Y}
+    if [[ "${user_input,,}" == "n" ]]; then
+        print_colored_v2 "INFO" "This process aborted by user."
+        exit 1
+    else
+        if [ -n "$suggested_action_cmd" ]; then
+            print_colored_v2 "INFO" "Suggested action will be performed."
+            eval "$suggested_action_cmd" || {
+                print_colored_v2 "ERROR" "Failed to perform suggested action."
+                exit 1
+            }
+        fi
+
+        if [ -n "$origin_cmd" ]; then
+            eval "$origin_cmd" || {
+                print_colored_v2 "ERROR" "${error_message}"
+                exit 1
+            }
+        fi
     fi
 }
